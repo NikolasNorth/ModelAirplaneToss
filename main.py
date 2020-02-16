dac7fe491c7d39fe8dbbf83733142d4a92880c09
Nikolas Anthony Teodoro North <nnorth2@uwo.ca>
2020-02-15 19:25:34 -0500

    Implement scrolling for background image

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 def welcome_screen():
     """
-    Display welcome screen to the user
+    Show welcome screen.
     :return:
     """
     # Position of intro title
@@ -17,11 +17,11 @@ def welcome_screen():
 
     # Position of subtitle
     original_subtitle_width = GAME_TITLES['default-secondary'].get_width()
-    orginal_subtitle_height = GAME_TITLES['default-secondary'].get_height()
+    original_subtitle_height = GAME_TITLES['default-secondary'].get_height()
     scaled_subtitle_width = 500
-    scaled_subtitle_height = int((scaled_subtitle_width / original_subtitle_width) * orginal_subtitle_height)
+    scaled_subtitle_height = int((scaled_subtitle_width / original_subtitle_width) * original_subtitle_height)
     subtitle_x_position = 150  # Center of window
-    substitle_y_position = 500
+    subtitle_y_position = 500
 
     # Position of model airplane
     airplane_x_position = int(SCREEN_WIDTH / 2) - 50  # Center of window
@@ -44,8 +44,9 @@ def welcome_screen():
                     (title_x_position, title_y_position)
                 )
                 SCREEN.blit(
-                    pygame.transform.scale(GAME_TITLES['default-secondary'], (scaled_subtitle_width, scaled_subtitle_height)),
-                    (subtitle_x_position, substitle_y_position)
+                    pygame.transform.scale(GAME_TITLES['default-secondary'],
+                                           (scaled_subtitle_width, scaled_subtitle_height)),
+                    (subtitle_x_position, subtitle_y_position)
                 )
                 SCREEN.blit(
                     pygame.transform.rotate(pygame.image.load(AIRPLANES['default']).convert_alpha(), 20),
@@ -56,12 +57,43 @@ def welcome_screen():
 
 
 def main_game():
-    print('Game has started!')
+    """
+    Run game.
+    :return:
+    """
+    # Set background image
+    background_x = 0  # Initial x-coordinate for 1st background image
+    background_x2 = SCREEN_WIDTH  # Initial x-coordinate for 2nd background image
+    SCREEN.fill((0, 0, 0))
     while True:
+        SCREEN.blit(
+            pygame.transform.scale(BACKGROUNDS['default'], (SCREEN_WIDTH, SCREEN_HEIGHT)),
+            (background_x, 0)
+        )
+        SCREEN.blit(
+            pygame.transform.scale(BACKGROUNDS['default'], (SCREEN_WIDTH, SCREEN_HEIGHT)),
+            (background_x2, 0)
+        )
+        pygame.display.update()
+        background_x -= 2
+        background_x2 -= 2
+        if background_x < -SCREEN_WIDTH:
+            background_x = SCREEN_WIDTH
+        if background_x2 < -SCREEN_WIDTH:
+            background_x2 = SCREEN_WIDTH
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
+            if event.type == pygame.QUIT:
+                # Quit application if user closes the game window
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    # Go back to welcome screen if user presses ESCAPE key
+                    welcome_screen()
+                elif event.key == pygame.K_SPACE:
+                    print('* Airplane rises *')
+
+        FPS_CLOCK.tick(FPS)
 
 
 def get_building_obstacle(building_type='default'):
@@ -73,39 +105,42 @@ def get_building_obstacle(building_type='default'):
 
 
 def main():
+    # Initialize global variables
     global SCREEN
     global SCREEN_WIDTH
     global SCREEN_HEIGHT
-    global FRAMES_PER_SECOND
+    global FPS
+    global FPS_CLOCK
     global AIRPLANES
     global BACKGROUNDS
     global BUILDINGS
     global BIRDS
     global GAME_TITLES
 
+    # Initialize pygame
     pygame.init()
+
+    # Set global variables
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    FRAMES_PER_SECOND = 30
-
+    FPS = 30
+    FPS_CLOCK = pygame.time.Clock()
     AIRPLANES = {
         'default': 'assets/user-airplane-1.png'
     }
-
     BACKGROUNDS = {
-        'default': ''
+        'default': pygame.image.load('assets/background-1.png').convert_alpha()
     }
-
     BUILDINGS = {
         'default': 'assets/building-1.png'
     }
-
     GAME_TITLES = {
         'default-main': pygame.image.load('assets/game-title-1.png').convert_alpha(),
         'default-secondary': pygame.image.load('assets/game-subtitle-1.png').convert_alpha()
     }
 
+    # Set caption and icon for game window
     pygame.display.set_caption("Model Airplane Toss")
     pygame.display.set_icon(pygame.image.load('assets/airplane-icon-1.png').convert_alpha())
 
