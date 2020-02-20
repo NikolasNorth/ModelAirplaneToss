18b7a67f2f6d110fd24928ff339113e102230e55
Nikolas Anthony Teodoro North <nnorth2@uwo.ca>
2020-02-19 19:19:24 -0500

    Add obstacle_img instance variable to class constructor and create getter method

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import os
 
 
 class Game:
-    def __init__(self, win_width, win_height, fps, bg_img, airplane_img):
+    def __init__(self, win_width, win_height, fps, bg_img, airplane_img, obstacle_img):
         """
         Game constructor.
         Initializes pygame, creates game window, creates Clock instance and loads image assets
@@ -13,6 +13,7 @@ class Game:
         :param fps: Frames per second
         :param bg_img: Background image file name (with extension)
         :param airplane_img: Airplane image file name (with extension)
+        :param obstacle_img: Obstacle image file name (with extension)
         """
         pygame.init()
         self.win_width = win_width
@@ -24,6 +25,7 @@ class Game:
             (win_width, win_height)
         )
         self.airplane_img = pygame.image.load(airplane_img).convert_alpha()
+        self.obstacle_img = pygame.image.load(obstacle_img).convert_alpha()
         self.clock = pygame.time.Clock()
 
     def get_window_width(self):
@@ -43,6 +45,9 @@ class Game:
 
     def get_airplane_img(self):
         return self.airplane_img
+
+    def get_obstacle_img(self):
+        return self.obstacle_img
 
     def get_clock(self):
         return self.clock
