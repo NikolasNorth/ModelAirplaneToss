7d9539266c99e9d1dc29bcef6d9382bc1ded2d51
Nikolas Anthony Teodoro North <nnorth2@uwo.ca>
2020-02-20 18:12:22 -0500

    Modify jump()

--- airplane.py
+++ airplane.py
@@ -11,8 +11,8 @@ class Airplane:
         Airplane constructor.
 
         :param img: airplane image loaded with pygame.image.load()
-        :param x: x-coordinate on game window
-        :param y: y-coordinate on game window
+        :param x: initial x-coordinate on game window
+        :param y: initial y-coordinate on game window
         """
         self.img = img
         self.x = x
@@ -36,6 +36,7 @@ class Airplane:
         self.tick_count = 0  # Restart counter
         self.velocity = -10.5
         self.height = self.y  # Original position prior to jump
+        self.move()
 
     def move(self):
         """
@@ -47,10 +48,10 @@ class Airplane:
         displacement = (self.velocity * self.tick_count) + (1.5 * self.tick_count ** 2)
 
         # Terminal velocity
-        if displacement >= 16:
-            displacement = 16
+        if displacement >= 12:
+            displacement = 12
 
-        # Fine tune displacement
+        # Fine tune upwards displacement
         if displacement < 0:
             displacement -= 2
 
